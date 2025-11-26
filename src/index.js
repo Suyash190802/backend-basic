d95d7625945ac95db132d8e540c164cf9e51a3a8
Suyash Choudhary <suyashchoudhary76@gmail.com>
2025-11-26 13:53:49 +0530

    ApiResponse and error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import  'dotenv/config';
 import connectDB from "./db/index.js" ;
 
 connectDB()
+.then(()=>{
+
+    app.on("error",(error)=>{
+    console.log("err:",error)
+  })
+app.listen(process.env.PORT || 8000 ,() => {
+ console.log(`Server is running on port ${process.env.PORT || 8000}`);
+})
+
+})
+.catch((error) => {
+  console.log("DB Connection Error:", error);
+})
 /*import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
 import express from "express";
